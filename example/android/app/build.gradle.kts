5aaaabdc2ad403e6c1a45664b556037911326275
Ilia Chirkunov <xc@yar.net>
2025-08-18 01:58:51 +0500

    Update build.gradle.kts

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.cheebeez.radio_player_example"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
