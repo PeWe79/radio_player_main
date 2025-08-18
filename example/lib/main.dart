beeb334e842c3835c86aecb2fa091e82a86003ee
Ilia Chirkunov <xc@yar.net>
2025-08-18 18:28:12 +0500

    Add iOS-compatible URL for visualizer

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -46,6 +46,7 @@ class _RadioPlayerExampleState extends State<RadioPlayerExample> {
     RadioPlayer.setStation(
       title: 'Radio Player',
       url: 'http://stream-uk1.radioparadise.com/aac-320',
+      //url: 'http://ice1.somafm.com/groovesalad-128-mp3',
       logoAssetPath: 'assets/cover.jpg',
     );
 
